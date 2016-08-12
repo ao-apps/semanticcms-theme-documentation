27099bdd2f9bc40d9e23b481767e9785180e1e7d
AO Industries, Inc <support@aoindustries.com>
2016-08-12 03:28:15 -0500

    Initial import

---
+++ src/main/java/com/semanticcms/theme/documentation/DocumentationTheme.java
@@ -0,0 +1,76 @@
+/*
+ * semanticcms-theme-documentation - SemanticCMS theme tailored for technical documentation.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-theme-documentation.
+ *
+ * semanticcms-theme-documentation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-theme-documentation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-theme-documentation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.theme.documentation;
+
+import com.aoindustries.servlet.http.Dispatcher;
+import com.semanticcms.core.model.Page;
+import com.semanticcms.core.servlet.Theme;
+import com.semanticcms.core.servlet.View;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.SkipPageException;
+
+/**
+ * A theme designed for technical documentation.
+ */
+public class DocumentationTheme extends Theme {
+
+	static final String THEME_NAME = "semanticcms-theme-documentation";
+
+	private static final String JSPX_TARGET = "/semanticcms-theme-documentation/theme.inc.jspx";
+
+	@Override
+	public String getDisplay() {
+		return "SemanticCMS Documentation";
+	}
+
+	@Override
+	public String getName() {
+		return THEME_NAME;
+	}
+
+	@Override
+	public void doTheme(
+		ServletContext servletContext,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		View view,
+		Page page
+	) throws ServletException, IOException, SkipPageException {
+		Map<String,Object> args = new LinkedHashMap<String,Object>();
+		args.put("view", view);
+		args.put("page", page);
+		Dispatcher.forward(
+			servletContext,
+			JSPX_TARGET,
+			request,
+			response,
+			args
+		);
+	}
+}
---
+++ src/main/java/com/semanticcms/theme/documentation/DocumentationThemeContextListener.java
@@ -0,0 +1,42 @@
+/*
+ * semanticcms-theme-documentation - SemanticCMS theme tailored for technical documentation.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-theme-documentation.
+ *
+ * semanticcms-theme-documentation is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-theme-documentation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-theme-documentation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.theme.documentation;
+
+import com.semanticcms.core.servlet.SemanticCMS;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener("Registers the \"" + DocumentationTheme.THEME_NAME + "\" theme in SemanticCMS.")
+public class DocumentationThemeContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		SemanticCMS.getInstance(event.getServletContext()).addTheme(new DocumentationTheme());
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// Do nothing
+	}
+}
