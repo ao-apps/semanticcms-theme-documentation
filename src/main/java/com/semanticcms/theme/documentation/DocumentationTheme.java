7326c1eedb768827856581706d03508414a675e3
AO Industries, Inc <support@aoindustries.com>
2020-02-26 23:55:26 -0600

    Optimized ServletContext attribute initialization and concurrency.

--- src/main/java/com/semanticcms/theme/documentation/DocumentationTheme.java
+++ src/main/java/com/semanticcms/theme/documentation/DocumentationTheme.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-theme-documentation - SemanticCMS theme tailored for technical documentation.
- * Copyright (C) 2016, 2017, 2018, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2018, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,15 +22,36 @@
  */
 package com.semanticcms.theme.documentation;
 
+import static com.aoindustries.net.Path.SEPARATOR_CHAR;
+import com.aoindustries.net.pathspace.Prefix;
+import static com.aoindustries.net.pathspace.Prefix.GREEDY_SUFFIX;
+import static com.aoindustries.net.pathspace.Prefix.WILDCARD_SUFFIX;
+import static com.aoindustries.net.pathspace.Prefix.valueOf;
+import com.aoindustries.servlet.firewall.pathspace.FirewallComponent;
+import com.aoindustries.servlet.firewall.pathspace.FirewallPathSpace;
+import com.aoindustries.servlet.firewall.pathspace.Rules;
+import static com.aoindustries.servlet.firewall.rules.Rules.chain.doFilter;
+import static com.aoindustries.servlet.firewall.rules.Rules.request.dispatcherType.isError;
+import static com.aoindustries.servlet.firewall.rules.Rules.request.dispatcherType.isForward;
+import static com.aoindustries.servlet.firewall.rules.Rules.request.dispatcherType.isInclude;
+import static com.aoindustries.servlet.firewall.rules.Rules.request.dispatcherType.isRequest;
+import static com.aoindustries.servlet.firewall.rules.Rules.request.method.GET;
+import static com.aoindustries.servlet.firewall.rules.Rules.request.method.constrain;
+import static com.aoindustries.servlet.firewall.rules.Rules.response.sendError.FORBIDDEN;
+import static com.aoindustries.servlet.firewall.rules.Rules.response.sendError.NOT_FOUND;
 import com.aoindustries.servlet.http.Dispatcher;
 import com.semanticcms.core.model.Page;
+import com.semanticcms.core.renderer.html.HtmlRenderer;
 import com.semanticcms.core.renderer.html.Theme;
 import com.semanticcms.core.renderer.html.View;
 import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.SkipPageException;
@@ -40,14 +61,127 @@ import javax.servlet.jsp.SkipPageException;
  */
 public class DocumentationTheme extends Theme {
 
-	static final String THEME_NAME = "semanticcms-theme-documentation";
+	private static final String NAME = "semanticcms-theme-documentation";
 
-	static final String PREFIX = "/" + THEME_NAME;
+	private static final String PREFIX = "/" + NAME;
 
 	private static final String JSPX_TARGET = PREFIX + "/theme.jspx";
 
 	// TODO: Version from filtered .xml with maven properties
-	static final String YUI_VERSION = "2.9.0";
+	private static final String YUI_VERSION = "2.9.0";
+
+	@WebListener("Registers the \"" + NAME + "\" theme and required scripts in HtmlRenderer and SemanticCMS.")
+	public static class Initializer implements ServletContextListener {
+
+		@Override
+		public void contextInitialized(ServletContextEvent event) {
+			ServletContext servletContext = event.getServletContext();
+			HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(servletContext);
+			// TODO: Return a Script object type instead, with a follow-up of "jQuery.noConflict();"
+			htmlRenderer.addScript("jquery", "/webjars/jquery/" + Maven.properties.getProperty("jquery.version") + "/jquery.min.js");
+			htmlRenderer.addTheme(new DocumentationTheme());
+			// TODO: Move to /META-INF/semanticcms-servlet-space.xml?
+			// TODO: Allow semanticcms-servlet-space.xml anywhere in the directory structure?
+			FirewallPathSpace.getFirewallPathSpace(servletContext).add(
+				// /semanticcms-theme-documentation/*
+				FirewallComponent.newInstance(
+					valueOf(PREFIX + WILDCARD_SUFFIX),
+					// /navigation.js as GET on request only
+					isRequest(
+						// If ever have more than one .js file, could use endsWith .js, or "map" to avoid sequential scan
+						Rules.pathMatch.path.equals("/navigation.js",
+							constrain(GET),
+							doFilter
+						),
+						// 404 everything else on "REQUEST" dispatcher
+						NOT_FOUND
+					),
+					// *.inc.jspx as include only
+					isInclude(
+						Rules.pathMatch.path.endsWith(".inc.jspx", doFilter)
+					),
+					// *.jspx as forward only, but not including *.inc.jspx
+					isForward(
+						Rules.pathMatch.path.endsWith(".jspx",
+							Rules.pathMatch.path.endsWith(".inc.jspx", FORBIDDEN),
+							doFilter
+						)
+					),
+					// Drop everything else
+					FORBIDDEN
+				),
+				// /semanticcms-theme-documentation/error-pages/*
+				FirewallComponent.newInstance(
+					valueOf(PREFIX + SEPARATOR_CHAR + "error-pages" + WILDCARD_SUFFIX),
+					// 404 everything on "REQUEST" dispatcher
+					isRequest(FORBIDDEN),
+					// Allow via "ERROR" dispatcher
+					isError(doFilter),
+					// Drop everything else
+					FORBIDDEN
+				),
+				// /semanticcms-theme-documentation/error-pages/jslib/yui-(version)/*** (greedy)
+				FirewallComponent.newInstance(
+					valueOf(PREFIX + SEPARATOR_CHAR + "jslib" + SEPARATOR_CHAR + "yui-" + YUI_VERSION + GREEDY_SUFFIX),
+					// Limit file exensions served on request dispatcher
+					isRequest(
+						// Block access to *.inc.jspx
+						Rules.pathMatch.path.endsWith(".inc.jspx", NOT_FOUND),
+						// Restrict all other to "GET"
+						constrain(GET),
+						doFilter
+					),
+					// *.inc.jspx as include only (different style than above)
+					isInclude(
+						Rules.pathMatch.path.endsWith(".inc.jspx", doFilter)
+					),
+					// Drop everything else
+					FORBIDDEN
+				),
+				// /images/*
+				// TODO: Move to docs-taglib:
+				FirewallComponent.newInstance(
+					// TODO: Use String[] overload of newInstance, once it exists
+					new Prefix[] {
+						valueOf("/images/*"),
+						valueOf("/images/list-item/16x16/*"),
+						valueOf("/styles/*")
+					},
+					// Constraint REQUEST dispatcher to GET only
+					isRequest(
+						constrain(GET),
+						doFilter
+					),
+					// Drop everything else
+					FORBIDDEN
+				),
+				// /semanticcms-theme-documentation/images/*
+				// /semanticcms-theme-documentation/styles/*
+				// TODO: Move to -styles project
+				FirewallComponent.newInstance(
+					// TODO: Use String[] overload of newInstance, once it exists
+					new Prefix[] {
+						valueOf(PREFIX + SEPARATOR_CHAR + "images" + WILDCARD_SUFFIX),
+						valueOf(PREFIX + SEPARATOR_CHAR + "styles" + WILDCARD_SUFFIX)
+					},
+					// Constraint REQUEST dispatcher to GET only
+					isRequest(
+						constrain(GET),
+						doFilter
+					),
+					// Drop everything else
+					FORBIDDEN
+				)
+			);
+		}
+
+		@Override
+		public void contextDestroyed(ServletContextEvent event) {
+			// Do nothing
+		}
+	}
+
+	private DocumentationTheme() {}
 
 	@Override
 	public String getDisplay() {
@@ -56,7 +190,7 @@ public class DocumentationTheme extends Theme {
 
 	@Override
 	public String getName() {
-		return THEME_NAME;
+		return NAME;
 	}
 
 	@Override
